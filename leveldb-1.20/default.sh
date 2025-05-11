a28828959f811cc22f00be541c1193bd7b0f01b9
hamli <haolinli@20.fudan.edu.cn>
2025-05-11 23:16:31 +0800

    新增测试脚本

--- leveldb-1.20/default.sh
+++ leveldb-1.20/default.sh
@@ -1,6 +1,6 @@
 
-export cores=(1 2 4 8 12 16 20 28)
-export mutex_cores=(1 2 4 8 12 16 20 28)
+export cores=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28)
+export mutex_cores=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28)
 export ncores=28
 export root_dir=/home/haoling/TCLocks
 export results_dir=${root_dir}/doc/results
